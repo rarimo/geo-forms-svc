118abbfb1a9b220f5dcd52c2ba30166bd12011b6
Zaptoss <zaptosgg@gmail.com>
2024-07-18 18:53:13 +0300

    Fix migrations

--- internal/assets/migrations/002_index.sql
+++ internal/assets/migrations/002_index.sql
@@ -1,7 +1,7 @@
 -- +migrate Up
 
-CREATE INDEX IF NOT EXISTS balances_status_index ON balances (status);
-CREATE INDEX IF NOT EXISTS balances_nullifier_index ON balances (nullifier);
+CREATE INDEX IF NOT EXISTS forms_status_index ON forms (status);
+CREATE INDEX IF NOT EXISTS forms_nullifier_index ON forms (nullifier);
 
 -- +migrate Down
 
