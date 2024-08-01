2cef884934eb5389360780c0d284b36137559d3d
Zaptoss <zaptosgg@gmail.com>
2024-08-01 11:07:20 +0300

    Remove commented code

--- internal/config/forms.go
+++ internal/config/forms.go
@@ -1,18 +1,12 @@
 package config
 
 import (
-	"database/sql"
 	"fmt"
 	"time"
 
-	"github.com/Masterminds/squirrel"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/rarimo/geo-forms-svc/internal/data"
 	"gitlab.com/distributed_lab/figure/v3"
 	"gitlab.com/distributed_lab/kit/kv"
 )
-
-const formsTable = "consulate"
 
 type Forms struct {
 	Cooldown          time.Duration
@@ -20,7 +14,6 @@ type Forms struct {
 	MinAbnormalPeriod time.Duration
 	MaxAbnormalPeriod time.Duration
 	ResendFormsCount  uint64
-	db                *sql.DB
 }
 
 func (c *config) Forms() *Forms {
@@ -41,11 +34,6 @@ func (c *config) Forms() *Forms {
 			panic(fmt.Errorf("failed to figure out withdrawal point price: %w", err))
 		}
 
-		// db, err := sql.Open("mysql", cfg.URL)
-		// if err != nil {
-		// 	panic(fmt.Errorf("failed to connect to mysql: %w", err))
-		// }
-
 		return &Forms{
 			Cooldown:          cfg.Cooldown,
 			Period:            cfg.Period,
@@ -54,61 +42,4 @@ func (c *config) Forms() *Forms {
 			ResendFormsCount:  cfg.ResendFormsCount,
 		}
 	}).(*Forms)
-}
-
-// SendForms inserts the forms to another remote database, which URL is provided in config.
-func (f *Forms) SendForms(forms ...*data.Form) error {
-	if len(forms) == 0 {
-		return nil
-	}
-
-	stmt := squirrel.Insert(formsTable).Columns(
-		"name",
-		"surname",
-		"id_num",
-		"birthday",
-		"citizen",
-		"visited",
-		"purpose",
-		"country",
-		"city",
-		"address",
-		"postal",
-		"phone",
-		"email",
-		"image",
-	)
-
-	for _, form := range forms {
-		if form == nil {
-			continue
-		}
-		stmt = stmt.Values(
-			form.Name,
-			form.Surname,
-			form.IDNum,
-			form.Birthday,
-			form.Citizen,
-			form.Visited,
-			form.Purpose,
-			form.Country,
-			form.City,
-			form.Address,
-			form.Postal,
-			form.Phone,
-			form.Email,
-			form.Image,
-		)
-	}
-
-	query, args, err := stmt.ToSql()
-	if err != nil {
-		return fmt.Errorf("failed to construct db query: %w", err)
-	}
-
-	if _, err = f.db.Exec(query, args...); err != nil {
-		return fmt.Errorf("insert form: %w", err)
-	}
-
-	return nil
 }
